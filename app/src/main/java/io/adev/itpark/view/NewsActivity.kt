e3175be4b6969d10d260bccadc450945f549b9cc
adev_one <a-dminator@hotmail.com>
2017-07-07 20:08:01 +0500

    - получение новостей с помощью rxjava

--- app/src/main/java/io/adev/itpark/view/NewsActivity.kt
+++ app/src/main/java/io/adev/itpark/view/NewsActivity.kt
@@ -3,9 +3,11 @@ package io.adev.itpark.view
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import com.google.gson.JsonParser
 import io.adev.itpark.R
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_news.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -22,53 +24,49 @@ class NewsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
-        val client = OkHttpClient.Builder()
-                .readTimeout(1000L,TimeUnit.MILLISECONDS)
-                .build()
-
-        val task = Runnable {
-
-            val request = Request.Builder()
-                    .url("https://api.myjson.com/bins/a829r")
-                    .build()
-
-            val result = client.newCall(request)
-                    .execute()
-                    .body().string()
-
-            val resultJson = JsonParser().parse(result).asJsonObject
-            val code = resultJson["code"].asInt
-
-            val news = mutableListOf<New>()
-
-            val responseArray = resultJson["response"].asJsonArray
-            for (el in responseArray) {
-                val newJson = el.asJsonObject
-
-//                val ПОЛЕ = нужныйJson["ПОЛЕ"].asНужныйТип
-
-                // пример
-                val title = newJson["title"].asString
-                val text  = newJson["text"].asString
-                val image = newJson["image"].asString
-
-                news.add(
-                        New(title, text, image))
-            }
-
-            runOnUiThread {
-                val adapter = NewsAdapter()
-                adapter.setData(news)
-                recyclerView.adapter = adapter
-                recyclerView.layoutManager = LinearLayoutManager(this)
-            }
-
-            Log.e("NewsActivity", result)
-        }
-
-        Thread(task).start()
-
         //создали в котлине список данных  который мы будем отображать(список новостей)
+
+        Observable.create<List<New>>
+                { emitter ->
+
+                    val client = OkHttpClient.Builder()
+                            .readTimeout(1000L,TimeUnit.MILLISECONDS)
+                            .build()
+
+                    val request = Request.Builder()
+                            .url("https://api.myjson.com/bins/a829r")
+                            .build()
+
+                    val result = client.newCall(request)
+                            .execute()
+                            .body().string()
+
+                    val resultJson = JsonParser().parse(result).asJsonObject
+                    val code = resultJson["code"].asInt
+
+                    val responseArray = resultJson["response"].asJsonArray
+                    val news =
+                            responseArray.map { el ->
+                                val newJson = el.asJsonObject
+
+                                val title = newJson["title"].asString
+                                val text  = newJson["text"].asString
+                                val image = newJson["image"].asString
+
+                                New(title, text, image)
+                            }
+
+                    emitter.onNext(news)
+                }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { news ->
+                    val adapter = NewsAdapter()
+                    adapter.setData(news)
+                    recyclerView.adapter = adapter
+                    recyclerView.layoutManager = LinearLayoutManager(this)
+                }
+
 
     }
 
